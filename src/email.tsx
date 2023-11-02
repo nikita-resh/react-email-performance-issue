41ac8979c93302a98503d983eef2454a7f0e6a04
nikita-resh <nikita.reshetnikov9@gmail.com>
2023-11-02 20:46:01 +0100

    0.0.11 + tailwind

--- src/email.tsx
+++ src/email.tsx
@@ -19,12 +19,14 @@ export const EmailVerificationEmail = () => (
     <Preview>Your login code for the app</Preview>
     <Tailwind>
       <Body>
-        <Container>
-          <Heading>Verify registration at app</Heading>
+        <Container className="mx-auto py-5 pt-20 pb-48 max-w-screen-xs">
+          <Heading className="text-2xl leading-5 font-normal text-gray-800 pt-4">
+            Verify registration at app
+          </Heading>
           <Section>
             <Button href="https://linear.app">Confirm email</Button>
           </Section>
-          <Text>
+          <Text className="mb-4 text-base leading-5 text-gray-700">
             If you have not recently created an account on SOME_APP, please feel
             free to disregard this email. If you have any concerns or questions,
             don't hesitate to reach out to our support team.
